99fe41045e7528b967a886cb19379e39bc6f1b49
Alexander Brockhaus <Alexander.Brockhaus@nonconvex.ch>
2020-08-30 10:49:45 +0200

    Add app settings and configuration f. db connection

---
+++ NonConvexWebsite/SqlDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonConvexWebsite
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+        }
+        public DataSet GetDs(string cmd)
+        {
+            DataSet ds = new DataSet();
+            return ds;
+        }
+
+    }
+}
--- NonConvexWebsite/Startup.cs
+++ NonConvexWebsite/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NonConvexWebsite.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace NonConvexWebsite
 {
@@ -29,6 +30,7 @@ namespace NonConvexWebsite
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
