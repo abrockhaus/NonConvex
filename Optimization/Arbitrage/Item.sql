f6fffa028860206a7fc4b9a9816139e98873dc8c
Alexander Brockhaus <Alexander.Brockhaus@nonconvex.ch>
2020-08-30 11:21:07 +0200

    Add Item Table, Sequence

---
+++ Optimization/Arbitrage/Item.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [Arbitrage].[Item]
+(
+	[ID] BIGINT NOT NULL PRIMARY KEY constraint DF_Arbitrage_Item_ID default next value for Arbitrage.SeqMain, 
+    [Label] NVARCHAR(80) NOT NULL
+)
